5c57b6eb374eea9327ef22a393508ba64202f21d
81909 <81909@DESKTOP-OAII446>
2021-03-22 11:08:00 +0900

    提交

--- src/main/java/com/dev/warehouse/bus/userinfo/api/UserInfoApi.java
+++ src/main/java/com/dev/warehouse/bus/userinfo/api/UserInfoApi.java
@@ -76,7 +76,6 @@ public class UserInfoApi {
             if(!StringUtil.lengthCheck(password, 6, 16)){
             	return new ResponseBean(ConstInfo.ERROR, "パスワードは6-16桁を入力してください", null, null);
     		}
-            
             //ユーザー新規登録処理(メール)を行います　START
             userInfo.setMailAddress(mailAddress);
             //パスワード暗号化処理 START
